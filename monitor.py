6e41734fa74c6c678570525addfdd1cb8b209117
ZizouFCB <ghazi.akoubii@gmail.com>
2026-09-26 15:30:56 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -30,9 +30,6 @@ def get_page(url):
     
     request_url = url
 
-    if "kicker.de" in url:
-        request_url = f"https://r.jina.ai/{url}"
-
     if "sport.sky.de" in url:
         request_url = f"https://r.jina.ai/{url}"
 
